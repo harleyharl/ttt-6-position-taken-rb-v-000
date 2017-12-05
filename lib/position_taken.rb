69c2bea7be27ce3b3ffe9637fa9542a52cd271cc
Harley Rayner <harleymteden@gmail.com>
2017-12-05 06:37:56 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -10,9 +10,9 @@ end
 
 # put more simply like this because if it is X or O, it won't be any of the other options:
 
-def position_taken?(board, index)
- 	board[index] == "X" || board[index] == "O"
-end
+# def position_taken?(board, index)
+#  	board[index] == "X" || board[index] == "O"
+# end
 
 # even more simply
-["X","O"].include?(board[index])
+# ["X","O"].include?(board[index])
